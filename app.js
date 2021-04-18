ce6806187a3094b0c1f0b0054d68eb351087d2b0
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-04-18 10:48:29 +0530

    Upgrade to embeds/attachments (#5)

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const Discord = require('discord.js');
 const schedule = require('node-schedule');
-const { getRandomPic } = require('./lib/pic');
 require('dotenv').config();
 
-const { getRandomQuote } = require('./lib/quotes');
-const { sendMessageToAllGuilds } = require('./lib/util');
-
+const { getEmbed } = require('./lib/quotes');
+const { sendMessageToAllGuilds, getWelcomeMessage } = require('./lib/util');
 const client = new Discord.Client();
-
-const unsplashToken = process.env.UNSPLASH_ACCESS_KEY;
 
 client.on('ready', async () => {
   console.log('Bot is ready');
@@ -19,22 +15,22 @@ client.on('ready', async () => {
   });
 
   const morningRule = new schedule.RecurrenceRule();
-  morningRule.hour = 06;
+  morningRule.hour = 6;
   morningRule.minute = 0;
   morningRule.tz = 'Asia/Kolkata';
 
   const nightRule = new schedule.RecurrenceRule();
   nightRule.hour = 23;
-  nightRule.minute = 00;
+  nightRule.minute = 0;
   nightRule.tz = 'Asia/Kolkata';
 
   schedule.scheduleJob(morningRule, async function () {
     try {
-      const quote = await getRandomQuote();
+      const embed = await getEmbed();
       const gm = ':sunrise_over_mountains:';
 
       sendMessageToAllGuilds(client, gm);
-      sendMessageToAllGuilds(client, quote);
+      sendMessageToAllGuilds(client, embed);
     } catch (err) {
       console.log('Could not send message to a (few) guild(s)!');
     }
@@ -42,10 +38,10 @@ client.on('ready', async () => {
 
   schedule.scheduleJob(nightRule, async function () {
     try {
-      const quote = await getRandomQuote();
+      const embed = await getEmbed();
       const gn = ':night_with_stars:';
 
-      sendMessageToAllGuilds(client, quote);
+      sendMessageToAllGuilds(client, embed);
       sendMessageToAllGuilds(client, gn);
     } catch (err) {
       console.log('Could not send message to a (few) guild(s)!');
@@ -59,31 +55,33 @@ client.on('message', async (msg) => {
   console.log('Message');
 
   if (msg.content === '!quote') {
-    const quote = await getRandomQuote();
+    try {
+      const embed = await getEmbed(msg);
 
-    if (!quote) {
-      msg.channel.send('An error occured. Please try again.');
-    } else {
-      msg.channel.send(quote);
+      await msg.channel.send(embed);
+    } catch (e) {
+      console.log(e);
+
+      await msg.channel.send('An error occured. Please try again.');
     }
   }
 });
 
-client.on('guildCreate', (guild) => {
+client.on('guildCreate', async (guild) => {
   let channelID;
-  let channels = guild.channels.cache;
+  const channels = guild.channels.cache;
 
-  for (let key in channels) {
-    let c = channels[key];
+  for (const key in channels) {
+    const c = channels[key];
     if (c[1].type === 'text') {
       channelID = c[0];
       break;
     }
   }
 
-  let channel = guild.channels.cache.get(guild.systemChannelID || channelID);
+  const channel = guild.channels.cache.get(guild.systemChannelID || channelID);
 
-  channel.send('Thanks for inviting me onto your server!');
-  channel.send('You can type **!quote** to get a random quote.');
-  channel.send('Carpe diem! :sparkles:');
+  const embed = await getWelcomeMessage();
+
+  await channel.send(embed);
 });
